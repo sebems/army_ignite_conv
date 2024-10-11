e8b3156de0893c94924e4e87a78a7c1ed760122b
Sean Ebenmelu <jindusean@gmail.com>
2024-10-11 09:54:43 -0400

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+from xml_conversion import export_xml
+
+st.title("CSV to XML Conversion")
+
+uploaded_file = st.file_uploader("Upload CSV File", type="csv")
+if uploaded_file is not None:
+    match uploaded_file.type:
+        case "text/csv":
+            try:
+                csv_df = pd.read_csv(
+                    uploaded_file, encoding="ascii", encoding_errors="ignore"
+                )
+                st.write(csv_df)
+
+                export_xml(csv_df.values.tolist())
+            except Exception as err:
+                st.error(type(err))  # the exception type
+                st.error(err.args)  # arguments stored in .args
+                st.error(err)
+
+            st.success("CSV file has been converted to XML format")
+        case _:
+            st.error("File given is not a CSV: ", uploaded_file.type)
+            # redundant since file_uploader only takes files of csv
+
+st.divider()
+
+with open("xml_export.xml", "r") as export_file:
+    st.download_button(
+        "Download File", data=export_file, file_name="xml_export.xml", mime="text/xml"
+    )
